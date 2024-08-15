cb134d6a0906054ec31dd0197205921f3dd002c5
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-08-15 20:00:40 -0300

    Edited app-routing.module

--- frontend/minimarket-app/src/app/app-routing.module.ts
+++ frontend/minimarket-app/src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-{ path: 'home', component: HomeComponent }
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
