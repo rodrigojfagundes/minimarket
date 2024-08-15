dd074b415004e7c36a36609bb04a5de2903d4ad5
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-08-15 19:38:10 -0300

    Developing form Category and Entity Category

--- frontend/minimarket-app/src/app/category/category-form/category-form.component.ts
+++ frontend/minimarket-app/src/app/category/category-form/category-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../category';
 
 @Component({
     selector: 'app-category-form',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
+    category: Category;
 
     constructor() {}
 
