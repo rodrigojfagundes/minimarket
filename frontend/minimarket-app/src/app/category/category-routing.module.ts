7c18de96a686360b84e6d5493a570099bb4e5977
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-08-15 19:36:41 -0300

    Create category-form folder

--- frontend/minimarket-app/src/app/category/category-routing.module.ts
+++ frontend/minimarket-app/src/app/category/category-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 const routes: Routes = [
-    
+    { path: 'category-form', component: CategoryFormComponent }
 ];
 
 
--- frontend/minimarket-app/src/app/category/category.module.ts
+++ frontend/minimarket-app/src/app/category/category.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from   '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { CategoryRoutingModule } from "./category-routing.module"; 
-
+import { CategoryRoutingModule } from './category-routing.module'; 
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [CategoryFormComponent],
     imports:[
         CommonModule,
         CategoryRoutingModule
     ],
     exports:[
-        
+        CategoryFormComponent
     ]
 })
 export class CategoryModule { }
