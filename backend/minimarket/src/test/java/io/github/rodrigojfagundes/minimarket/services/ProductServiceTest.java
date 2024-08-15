b5c884b177ac55d2b7ac165a10b1c55d3f5ba59e
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-08-15 18:49:49 -0300

    ProductService - Insert tested

---
+++ backend/minimarket/src/test/java/io/github/rodrigojfagundes/minimarket/services/ProductServiceTest.java
@@ -0,0 +1,89 @@
+package io.github.rodrigojfagundes.minimarket.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.*;
+
+import io.github.rodrigojfagundes.minimarket.dto.CategoryDTO;
+import io.github.rodrigojfagundes.minimarket.dto.ProductDTO;
+import io.github.rodrigojfagundes.minimarket.entities.Category;
+import io.github.rodrigojfagundes.minimarket.entities.Product;
+import io.github.rodrigojfagundes.minimarket.repositories.CategoryRepository;
+import io.github.rodrigojfagundes.minimarket.repositories.ProductRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+	
+	@Mock
+	private ProductRepository repository;
+	
+	@Mock
+	private CategoryRepository categoryRepository;
+	
+	@InjectMocks
+	private ProductService service;
+	
+	private Product product0;
+	
+	private ProductDTO productDTO0;
+	
+	private Category category0;
+	
+	private CategoryDTO categoryDTO0;
+	
+	
+	@BeforeEach
+	public void setup() {
+		category0 = new Category(1L,
+				"name1",
+				"description1");
+		
+		categoryDTO0 = new CategoryDTO(1L,
+				"name1",
+				"description1");
+		
+		product0 = new Product(null,
+				"name1",
+				"description",
+				1L, category0);		
+				
+		productDTO0 = new ProductDTO(1L,
+				"name",
+				"description",
+				1L, 
+				anyLong()
+				);		
+	}
+	
+	@DisplayName("JUnit test for Given Product Object When Save product then return Product Object")
+	@Test
+	void testGivenProductObject_WhenSaveProduct_thenReturnProductObject() {
+		
+		
+		//Given / Arrange
+		given(categoryRepository.getOne(productDTO0.getCategoryId())).willReturn(category0);
+		given(repository.save(product0)).willReturn(product0);
+		//Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product0);
+		
+		//When / Act
+		ProductDTO savedProductDTO = service.inset(productDTO0);
+		
+		//Then / Assert
+		assertNotNull(savedProductDTO);
+		assertEquals("name1", savedProductDTO.getName());
+		
+	}
+	
+}
