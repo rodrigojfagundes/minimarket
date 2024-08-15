3d370bd2f76a31d8563bfe2a6463effaefb0016c
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-08-15 20:06:56 -0300

    Created layout

---
+++ frontend/minimarket-app/src/app/layout/layout.component.ts
@@ -0,0 +1,29 @@
+import { Component, AfterViewInit } from '@angular/core';
+import jQuery from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.ts'],
+})
+export class AppComponent implements AfterViewInit {
+  title = 'minimarket-app';
+
+  ngAfterViewInit(): void {
+    (function ($) {
+      'use strict';
+
+      var path = window.location.href;
+      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function () {
+        if (this.href === path) {
+          $(this).addClass('active');
+        }
+      });
+
+      $('#sidebarToggle').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('sb-sidenav-toggled');
+      });
+    })(jQuery);
+  }
+}
